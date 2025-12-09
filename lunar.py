102c3530a1386306b98568d58b3d593ddf84296a
Ann Elliott <elliott.222@osu.edu>
2025-12-08 19:22:07 -0700

    Converted to python3

--- lunar.py
+++ lunar.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 import collections
 
 import ephem
@@ -8,9 +8,9 @@ import config
 
 location = config.default_location
 moon = ephem.Moon(location)
-print "Current phase is %.2f%% illuminated" % (moon.phase)
-print "Moon in %s is at alt %s in %s to the %s" % (location.name, moon.alt, ephem.constellation(moon)[1], az2dir(moon.az))
-print
+print("Current phase is %.2f%% illuminated" % (moon.phase))
+print("Moon in %s is at alt %s in %s to the %s" % (location.name, moon.alt, ephem.constellation(moon)[1], az2dir(moon.az)))
+print()
 
 
 locations = {'rise': config.time_conversion(location.next_rising(moon)),
@@ -19,7 +19,7 @@ locations = {'rise': config.time_conversion(location.next_rising(moon)),
             }
     
 if not locations['transit']:
-    print 'No transit time'
+    print( 'No transit time')
     del locations['transit']
 
 o_loc = collections.OrderedDict(sorted(locations.items(), key=lambda t: t[1]))
@@ -32,16 +32,16 @@ phases = {"Full Moon": ephem.next_full_moon(location.date),
 
 o_phases = collections.OrderedDict(sorted(phases.items(), key=lambda t: t[1]))
 
-for what, when in o_loc.iteritems():
+for what, when in o_loc.items():
     location.date = when
     moon.compute(location)
-    print "The moon will %7s on %s to the %s in %s" % \
+    print( "The moon will %7s on %s to the %s in %s" % \
             (what, when.strftime('%c'), az2dir(moon.az),
-                    ephem.constellation(moon)[1])
-print 
+                    ephem.constellation(moon)[1]))
+print() 
 
-for what, when in o_phases.iteritems():
+for what, when in o_phases.items():
     location.date = when
     moon.compute(location)
-    print what.ljust(15), config.time_conversion(when).strftime('%c'), '%11s to the %-2s in %s' % (str(moon.alt), az2dir(moon.az), ephem.constellation(moon)[1])
+    print(what.ljust(15), config.time_conversion(when).strftime('%c'), '%11s to the %-2s in %s' % (str(moon.alt), az2dir(moon.az), ephem.constellation(moon)[1]))
 
